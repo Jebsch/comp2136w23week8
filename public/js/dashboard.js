d2eeb75aeb4fa966f90a5055db5d929331bd52ee
Jebsch <124074543+Jebsch@users.noreply.github.com>
2023-03-14 17:45:44 +0000

    commitment

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { reset } = require("nodemon");
+
 const $ = (selector) => document.querySelector(selector);
 
 const postalRegEx =
@@ -13,11 +15,18 @@ const onReset = (evt) => {
   $("#eco").checked = true;
   $("#temperature").value = 21;
   $("#location").value = "L7W 4T8";
+  resetErrors();
+  console.error("Fields reset");
 
   evt.preventDefault();
 };
+const resetErrors = ()=>{
+  $("#temperature_error").textContent="";
+  $("#location_error").textContent="";
+}
 
 const onSubmit = (evt) => {
+  resetErrors();
   //TODO::Reset any errors before submitting
 
   //TODO:: Set notifications since it doesn't need to be validated
@@ -42,12 +51,33 @@ const onSubmit = (evt) => {
 
   if (postalRegEx.test(location)){
     //if the postal code is valid this code will run
+
+    $("#setting_location").textcontent = location;
   }else{
     //Add your logic here if the postal code is not valid
+
+    $("#location_error").textcontent = "The postal code did not match the format required."
   }
     //TODO:: Validate the temperature by checking the range and if it's a number
     //TODO:: Display an error if not valid
-    evt.preventDefault();
+
+  let temperature = $("#temperature").value;
+  let temperatureError = $("#temperature_error");
+
+
+  if (isNaN(temperature) || temperature == ""){
+    temperatureError.textContent = "This is not a valid temperature selection.";
+  }else if (temperature>25 ){
+    temperatureError.textContent = "Please select temperature under 25 degrees Celcius.";
+    $("#setting_temperature").textContent = 25;
+  }else if ( temperature<10){
+    temperatureError.textContent = "Please select temperature over 10 degrees Celcius.";
+    $("#setting_temperature").textContent = 10;
+  }else {
+    $("#setting_temperature").textContent = temperature;
+  }
+    
+  evt.preventDefault();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
--- public/js/profile.js
+++ public/js/profile.js
@@ -7,8 +7,21 @@ const emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 const passwordRegEx = /^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$/;
 
 const onReset = (evt) => {
+  resetErrors();
   //TODO:: Reset the reset-able fields
+  $("first_name").value = "";
+  $("last_name").value = "";
+  $("email").value = "";
+  $("password").value = "";
+  $("confirm_password").value = "";
+  evt.preventDefault();
 };
+
+const resetErrors = () =>{
+  $("#name_error").textcontent = "";
+  $("#password_error").textcontent = "";
+  $("#email_error").textcontent = "";
+}
 
 const onSubmit = (evt) => {
   resetErrors();
@@ -16,6 +29,38 @@ const onSubmit = (evt) => {
   //TODO:: Use this boolean to keep track of any errors because you need to prevent the settings
   //       from updating if even one field is wrong
   let formErrors = false;
+  let firstname = $("#first_name");
+  let lastname = $("#last_name");
+  let email = $("#email");
+  let password = $("#password");
+  let confirm_password = $("#confirm_password");
+  let today = new Date();
+
+  if (firstname == ""){
+    $("#name_error").textcontent = "Name fields can't be empty."
+    formErrors = true;
+  }
+  if (lastname == ""){
+    $("#name_error").textcontent = "Try again Mclovin."
+    formErrors = true;
+  }
+  if (!emailRegEx.test(email)){
+    $("#email_error").textcontent = "Email invalid."
+    formErrors = true;
+  }
+  if (!passwordRegEx.test(password)){
+    $("#password_error").textcontent = "Password is invalid."
+    formErrors = true;
+  } 
+  if (password != confirm_password){
+    $("#password_error").textcontent = "Passwords do not match."
+    formErrors = true;
+  }
+  if (today.sethours (0,0,0,0) < dob){
+    $("#dob_error").textcontent
+    formErrors = true;
+  }
+
 
   //TODO:: Make sure name fields are not empty
 
@@ -27,7 +72,24 @@ const onSubmit = (evt) => {
   //TODO:: Make sure date of birth is in the past.
 
   //TODO:: check you formErrors boolean and update the fields if still false
+if (!formErrors) {
+  let dob = new Date($("#dob").value);
+  //$("#user_dob").textcontent = $("dob").value;
+  console.log(dob);
+  $("#userdob").textcontent = dob.toDateString();
 
+  
+  $("#user_password_last_changed").textcontent = today.toDateString();
+
+  $("#first_name").textcontent = $("#user_first_name").value;
+  $("#last_name").textcontent = $("#user_last_name").value;
+  $("#email").textcontent = $("#user_email").value;
+  
+
+
+  
+
+}
   evt.preventDefault();
 };
 
